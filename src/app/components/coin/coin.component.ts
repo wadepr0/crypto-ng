933980d32f16198d898ee743a62c0012ba238472
wadepr0 <67891615+wadepr0@users.noreply.github.com>
2022-07-30 22:22:27 +0300

    project init

---
+++ src/app/components/coin/coin.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ICoin } from 'src/app/types/ICoin';
+
+@Component({
+  selector: 'app-coin',
+  templateUrl: './coin.component.html',
+  styleUrls: ['./coin.component.scss']
+})
+export class CoinComponent implements OnInit {
+  @Input() coin!: ICoin;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
