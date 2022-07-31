0501bbf56de1e20424722b98d1173afea683072d
wadepr0 <67891615+wadepr0@users.noreply.github.com>
2022-07-31 22:57:08 +0300

    added nav-bar

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { delay, tap } from 'rxjs/operators';
+import { HttpService } from 'src/app/services/http.service';
+import { IRegisteredCoins } from 'src/app/types/IRegisteredCoins';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavBarComponent implements OnInit {
-
-  constructor() { }
+  @Input() currency!: string;
+  allCoins: IRegisteredCoins[] | null = null;
+  constructor(private httpService$: HttpService) { }
 
   ngOnInit(): void {
+    this.loadCoins();
+  }
+
+  loadCoins() {
+    this.httpService$.getAllRegisteredCoins()
+      .pipe(
+        delay(500),
+        // map((i) => i.filter((i: ICoin) => !i.id.includes('coin'))),
+        tap(i => console.log(i)
+        ))
+      .subscribe(res => {
+        this.allCoins = res;
+      })
+  }
+
+  trackByFn(index: any, item: any) {
+    return item.id; // or item.id
   }
 
 }
